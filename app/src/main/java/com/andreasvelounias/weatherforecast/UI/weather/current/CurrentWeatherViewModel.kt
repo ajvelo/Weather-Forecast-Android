599c82b700fdd658502e6a719f5082a0d26b4cb2
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-02-04 15:18:17 +0000

    Add skeleton fragments

---
+++ app/src/main/java/com/andreasvelounias/weatherforecast/UI/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.andreasvelounias.weatherforecast.UI.weather.current
+
+import androidx.lifecycle.ViewModel;
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/andreasvelounias/weatherforecast/UI/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.andreasvelounias.weatherforecast.UI.weather.future.list
+
+import androidx.lifecycle.ViewModel;
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
