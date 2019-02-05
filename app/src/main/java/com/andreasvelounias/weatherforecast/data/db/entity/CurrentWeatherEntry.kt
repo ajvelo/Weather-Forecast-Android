64968c1da7b54e0606da7f909039997c65f571cd
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-02-05 10:26:17 +0000

    Turn CurrentWeatherEntry into Entity  - add primary key
    Add unitspecificcurrentweatherentry interface

--- app/src/main/java/com/andreasvelounias/weatherforecast/data/db/entity/CurrentWeatherEntry.kt
+++ app/src/main/java/com/andreasvelounias/weatherforecast/data/db/entity/CurrentWeatherEntry.kt
@@ -1,8 +1,15 @@
 package com.andreasvelounias.weatherforecast.data.db.entity
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+const val CURRENT_WEATHER_ID = 0
+
+@Entity(tableName = "current_weather")
 data class CurrentWeatherEntry(
+    @Embedded(prefix = "condition_")
     val condition: Condition,
     @SerializedName("feelslike_c")
     val feelslikeC: Double,
@@ -29,4 +36,7 @@ data class CurrentWeatherEntry(
     val windKph: Double,
     @SerializedName("wind_mph")
     val windMph: Double
-)
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = CURRENT_WEATHER_ID
+}
